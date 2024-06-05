64204833a38b4a3dac6d18109b779f0d6730b102
Victor Nivelo <104579002+VictorNivelo@users.noreply.github.com>
2024-06-04 21:27:45 -0500

    Mejoras y diseño web

---
+++ PIS/Vistas.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def PaginaPrincipal(request):
+    return render(request, 'PaginaPrincipal.html')
+
+def Hola(request):
+    return render(request, 'HolaMundo.html')
+
+def p(request):
+    return render(request, 'index.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def full_width(request):
+    return render(request, 'full-width.html')
+
+def sidebar_left(request):
+    return render(request, 'sidebar-left.html')
+
+def sidebar_right(request):
+    return render(request, 'sidebar-right.html')
+
+def basic_grid(request):
+    return render(request, 'basic-grid.html')
--- PIS/urls.py
+++ PIS/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for PIS project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from .Vistas import PaginaPrincipal, Hola, p, gallery, full_width, sidebar_left, sidebar_right, basic_grid
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pr', PaginaPrincipal, name='PaginaPrincipal'),
+    path('Hola/', Hola, name='Hola'),
+    path('', p, name='index'),
+    path('gallery/', gallery, name='gallery'),
+    path('full-width/', full_width, name='full_width'),
+    path('sidebar-left/', sidebar_left, name='sidebar_left'),
+    path('sidebar-right/', sidebar_right, name='sidebar_right'),
+    path('basic-grid/', basic_grid, name='basic_grid'),
 ]
